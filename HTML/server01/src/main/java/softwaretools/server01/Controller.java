14328aac00bf8af59737dd4f93a1f64846004d80
MasonSyj <shiyujiesx@163.com>
2023-03-29 13:30:10 +0100

    split into html, css, js three parts

---
+++ HTML/server01/src/main/java/softwaretools/server01/Controller.java
@@ -0,0 +1,50 @@
+package softwaretools.server01;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpHeaders;
+
+@RestController
+public class Controller {
+
+    @Autowired
+    ResourceLoader loader;
+
+    @GetMapping("/")
+    public ResponseEntity<String> mainPage() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.CONTENT_TYPE, "text/plain");
+
+        return new ResponseEntity<String>("Hello from Spring", headers, 200);
+    }
+
+    @GetMapping("/html")
+    public ResponseEntity<Resource> htmlPage() {
+        Resource htmlfile = loader.getResource("classpath:web/page.html");
+        return ResponseEntity
+            .status(200)
+            .header(HttpHeaders.CONTENT_TYPE, "text/html")
+            .body(htmlfile);
+    }
+
+    @GetMapping("/index")
+    public ResponseEntity<Resource> indexPage() {
+        Resource htmlfile = loader.getResource("classpath:web/index.html");
+        return ResponseEntity
+                .status(200)
+                .header(HttpHeaders.CONTENT_TYPE, "text/html")
+                .body(htmlfile);
+    }
+
+    @GetMapping("/bad")
+    public ResponseEntity<String> badPage() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.CONTENT_TYPE, "text/plain");
+
+        return new ResponseEntity<String>("404 NOT FOUND", headers, 200);
+    }
+}

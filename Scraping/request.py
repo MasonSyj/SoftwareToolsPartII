aebf4d83b27e6b2a41ff40458626747e71845b91
MasonSyj <shiyujiesx@163.com>
2023-04-01 17:25:47 +0100

    hello world

--- Scraping/request.py
+++ Scraping/request.py
@@ -10,12 +10,20 @@ soup = BeautifulSoup(html_text, 'lxml')
 # jobs = soup.find_all('article', class_='job-result-card')
 jobs = soup.find_all('div', class_='col-xs-12 col-sm-8 col-md-9 details')
 
+# print(soup.find('div', class_='col-xs-12 col-sm-8 col-md-9 details'))
 # print(jobs)
 
 for job in jobs:
+    # print(job)
     title = job.find('h2', class_='job-result-heading__title')
     title_name = title.find('a').text
     salary = job.find('li', class_='job-metadata__item job-metadata__item--salary').text
+    link = job.find('a', class_='gtmJobListingPostedBy')
+    linkaddress = "Not Provided"
+    if 'href' in link.attrs:
+        linkaddress = link['href']
+
     print(title_name.strip())
     print(salary.strip())
+    print(linkaddress)
     print('-------------------------')

a064dbe297f6b6114ffcdf18924521fbb13fcdb5
MasonSyj <shiyujiesx@163.com>
2023-04-06 21:02:04 +0100

    first touch on json but question remained

---
+++ JS/basic/object.js
@@ -0,0 +1,9 @@
+const student = {
+    "name" : "sam",
+    "id": 2019214,
+    "age": 31
+}
+
+for (element in student){
+    console.log(element + " : " + student[element]);
+}
---
+++ JS/json/convert.js
@@ -0,0 +1,14 @@
+const obj = {
+    "name": "harry",
+    "age": 23,
+    "id": "djaaio"
+}
+
+const jsonF = JSON.stringify(obj);
+console.log("jsonFormat:" + jsonF);
+
+const jsonG = '{"name": "sam", "age": 34}';
+const objectised = JSON.parse(jsonG);
+console.log(objectised);
+console.log("object:" + objectised);
+console.log("object: " + "" + objectised.age);

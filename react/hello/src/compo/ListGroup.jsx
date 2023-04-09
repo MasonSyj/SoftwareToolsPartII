ce6b22865b9e9479598e647007ce5a75518666cf
MasonShi <shiyujiesx@163.com>
2023-04-09 15:49:39 +0100

    learn useState, hardly understand why use in this way

--- react/hello/src/compo/ListGroup.jsx
+++ react/hello/src/compo/ListGroup.jsx
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import { useState } from "react";
 
 function ListGroup() {
     let items = [
@@ -9,7 +10,9 @@ function ListGroup() {
         'Paris'
     ];
 
-    let CurrentIndex = 0;
+    const [CurrentIndex, setIndex] = useState(0);
+    const [clickCNT, incrementClickCNT] = useState(1);
+    const [name, setName] = useState('Your name');
 
     const getMessage = () => {
         return items.length === 0 ? <p>No item found</p> : null;
@@ -17,9 +20,19 @@ function ListGroup() {
 
     const handleClick = (item, index) => {
         console.log({item});
-        CurrentIndex = index;
+        setIndex(index);
     }
-        
+
+    const handleCNTClick = () => {
+        incrementClickCNT(clickCNT + 1);
+    }
+
+    const handleSetName = () => {
+        event.preventDefault();
+        var newName = document.getElementById("name").value;
+        console.log(newName);
+        setName(newName);
+    }
 
     return (
         <Fragment>
@@ -29,11 +42,19 @@ function ListGroup() {
                 {items.map((item, index) => 
                     <li 
                         className = { CurrentIndex === index ? 'list-group-item active' : 'list-group-item'}
-                        onClick={() => handleClick(item, index)} 
-                        key={item}>
-                            {item}
-                    </li>)}
+                        onClick = {() => handleClick(item, index)} 
+                        key = {item}
+                    >
+                    {item}
+                    </li>
+                )}
             </ul>
+            <button onClick={() => handleCNTClick()}>Click to change count</button>
+            <p>You clicked {clickCNT} times</p>
+                <label htmlFor="name"> Set your name: </label>
+                <input type="text" name="name" id="name" />
+                <input type="submit" value="submit" onClick={handleSetName}/>
+            <p> Your current name: {name}</p>
         </Fragment>
     )
      
